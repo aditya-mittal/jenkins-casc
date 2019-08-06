ca0660959da71c1f989cca8124aacddde2f3eca7
aditya <amittal@equalexperts.com>
2019-08-06 22:12:06 +0530

    Add seedJob in jenkins casc yml

--- jenkins-base/casc-configs/seed.groovy
+++ jenkins-base/casc-configs/seed.groovy
@@ -1,76 +1,36 @@
-pipelineJob('pipeline/seedjob') {
-    description("Auto-generated seed job")
-    definition {
-        cps {
-            script ("""
-                pipeline {
-                    agent any
-                    stages {
-                        stage('process-job-dsls') {
-                            steps {
-                                jobDsl scriptText: '''
-                                    pipelineJob('pipeline/seeded-pipeline-1') {
-                                      definition {
-                                        cps {
-                                          script (\"""
-                                            pipeline {
-                                                agent any
-                                                stages {
-                                                    stage('build') {
-                                                        steps {
-                                                            sh script: 'echo "Build"'
-                                                        }
-                                                    }
-                                                    stage('test') {
-                                                        steps{
-                                                            sh script : 'echo "Test"'
-                                                        }
-                                                    }
-                                                    stage('publish') {
-                                                        steps{
-                                                            sh script : 'echo "Publish"'
-                                                        }
-                                                    }
-                                                }
-                                            }\""".stripIndent()
-                                          )
-                                        }
-                                      }
-                                    }
-                                    pipelineJob('pipeline/seeded-pipeline-2') {
-                                      definition {
-                                        cps {
-                                          script (\"""
-                                            pipeline {
-                                                agent any
-                                                stages {
-                                                    stage('build') {
-                                                        steps {
-                                                            sh script: 'echo "Build"'
-                                                        }
-                                                    }
-                                                    stage('test') {
-                                                        steps{
-                                                            sh script : 'echo "Test"'
-                                                        }
-                                                    }
-                                                    stage('publish') {
-                                                        steps{
-                                                            sh script : 'echo "Publish"'
-                                                        }
-                                                    }
-                                                }
-                                            }\""".stripIndent()
-                                          )
-                                        }
-                                      }
-                                    }
-                                '''
-                            }
-                        }
-                    }
+freeStyleJob('seedjob1') {
+    steps {
+        jobDsl {
+            scriptText("""
+              pipelineJob('seeded-pipeline-1') {
+                definition {
+                  cps {
+                    script (\"""
+                      pipeline {
+                          agent any
+                          stages {
+                              stage('build') {
+                                  steps {
+                                      sh script: 'echo "Build"'
+                                  }
+                              }
+                              stage('test') {
+                                  steps{
+                                      sh script : 'echo "Test"'
+                                  }
+                              }
+                              stage('publish') {
+                                  steps{
+                                      sh script : 'echo "Publish"'
+                                  }
+                              }
+                          }
+                      }\""".stripIndent()
+                    )
+                  }
                 }
-            """)
+              }
+            """.stripIndent())
         }
     }
 }
